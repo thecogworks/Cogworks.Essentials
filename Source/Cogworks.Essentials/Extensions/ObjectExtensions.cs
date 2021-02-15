3019c6c8180001eaa890b0026cdd993ffee0a3f3
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-15 14:36:12 +0100

    feat: added object extensions

---
+++ Source/Cogworks.Essentials/Extensions/ObjectExtensions.cs
@@ -0,0 +1,8 @@
+namespace Cogworks.Essentials.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static bool HasValue(this object input)
+            => input != null;
+    }
+}
