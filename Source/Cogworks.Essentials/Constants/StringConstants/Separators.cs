4a648adbb9a354eb7f78f20d1e09bd06ec367dd5
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-15 13:16:26 +0100

    feat: added separators constants

---
+++ Source/Cogworks.Essentials/Constants/StringConstants/Separators.cs
@@ -0,0 +1,17 @@
+namespace Cogworks.Essentials.Constants.StringConstants
+{
+    public static class Separators
+    {
+        public const string Comma = ",";
+        public const string Space = " ";
+        public const string Underscore = "_";
+        public const string CommaAndSpace = Comma + Space;
+        public const string Slash = "/";
+        public const string Backslash = @"\";
+        public const string Dot = ".";
+        public const string Semicolon = ";";
+        public const string Assign = ":=";
+        public const string Vertical = "|";
+        public const string Hyphen = "-";
+    }
+}
