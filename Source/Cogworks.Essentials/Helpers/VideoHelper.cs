7c8501e896a2044ace54a4b39c40e98f30c82f80
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-16 09:27:27 +0100

    feat: updated video helper

--- Source/Cogworks.Essentials/Helpers/VideoHelper.cs
+++ Source/Cogworks.Essentials/Helpers/VideoHelper.cs
@@ -1,5 +1,7 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 using Cogworks.Essentials.Constants.StringConstants;
+using Cogworks.Essentials.Enums;
 using Cogworks.Essentials.Extensions;
 
 namespace Cogworks.Essentials.Helpers
@@ -11,6 +13,11 @@ namespace Cogworks.Essentials.Helpers
             if (!url.HasValue())
             {
                 return string.Empty;
+            }
+
+            if (!Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+            {
+                throw new ArgumentException("Not a valid URL");
             }
 
             var youTubeMatch = Regex.Match(url, SocialMediaConstants.RegexConstants.YouTube);
@@ -30,6 +37,34 @@ namespace Cogworks.Essentials.Helpers
             }
 
             return string.Empty;
+        }
+
+        public static VideoServiceType GetVideoServiceType(string url, out string videoId)
+        {
+            if (!Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+            {
+                throw new ArgumentException("Not a valid URL");
+            }
+
+            videoId = string.Empty;
+
+            var youTubeMatch = Regex.Match(url, SocialMediaConstants.RegexConstants.YouTube);
+
+            if (youTubeMatch.Success && youTubeMatch.Groups.Count > 1)
+            {
+                videoId = youTubeMatch.Groups[1].Value;
+                return VideoServiceType.YouTube;
+            }
+
+            var vimeoMatch = Regex.Match(url, SocialMediaConstants.RegexConstants.Vimeo);
+
+            if (vimeoMatch.Success && vimeoMatch.Groups.Count > 1)
+            {
+                videoId = vimeoMatch.Groups[1].Value;
+                return VideoServiceType.Vimeo;
+            }
+
+            return VideoServiceType.Unknown;
         }
     }
 }
