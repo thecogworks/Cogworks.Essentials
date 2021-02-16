225dd8bd0232c9f6677fe43ef46f1dd2e44802c3
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-16 09:27:42 +0100

    feat: added cache service

---
+++ Source/Cogworks.Essentials/Services/MemoryCacheService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Cogworks.Essentials.Services
+{
+    public class MemoryCacheService : ICacheService
+    {
+        private static MemoryCacheOptions Options => new MemoryCacheOptions();
+
+        private static MemoryCache Cache => new MemoryCache(Options);
+
+        public bool Contains(string cacheKey)
+            => Cache.TryGetValue(cacheKey, out _);
+
+        public T GetCacheItem<T>(string cacheKey)
+            => Cache.Get(cacheKey) is T cachedObject
+                ? cachedObject
+                : default;
+
+        public void RemoveCacheItem(string cacheKey)
+            => Cache.Remove(cacheKey);
+
+        public void SetCacheItem(string cacheKey, object value, int cacheDuration = 24 * 60 * 60)
+        {
+            var policy = new MemoryCacheEntryOptions() { AbsoluteExpiration = DateTime.Now.AddSeconds(cacheDuration) };
+
+            Cache.Set(cacheKey, value, policy);
+        }
+    }
+}
