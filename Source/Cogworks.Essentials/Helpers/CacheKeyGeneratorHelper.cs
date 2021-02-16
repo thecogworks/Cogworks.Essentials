513ec2259f5d3f64c50a8c6181f14c91762fb186
Adrian Ochmann <adrian.ochmann@outlook.com>
2021-02-16 09:10:32 +0100

    feat: added helpers

---
+++ Source/Cogworks.Essentials/Helpers/CacheKeyGeneratorHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Cogworks.Essentials.Helpers
+{
+    public static class CacheKeyGeneratorHelper
+    {
+        public static string GetCacheKey<TCachedProxy, TReturnedType>()
+        {
+            var cacheKey = $"{typeof(TCachedProxy)}_{typeof(TReturnedType)}";
+
+            return cacheKey;
+        }
+
+        public static string GetCacheKey<TCachedProxy, TReturnedType>(int nodeId)
+        {
+            var cacheKey = $"{typeof(TCachedProxy)}_{typeof(TReturnedType)}_{nodeId}";
+
+            return cacheKey;
+        }
+    }
+}
---
+++ Source/Cogworks.Essentials/Helpers/VideoHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.RegularExpressions;
+using Cogworks.Essentials.Constants.StringConstants;
+using Cogworks.Essentials.Extensions;
+
+namespace Cogworks.Essentials.Helpers
+{
+    public static class VideoHelper
+    {
+        public static string GetVideoEmbeddedUrl(string url)
+        {
+            if (!url.HasValue())
+            {
+                return string.Empty;
+            }
+
+            var youTubeMatch = Regex.Match(url, SocialMediaConstants.RegexConstants.YouTube);
+
+            if (youTubeMatch.Success)
+            {
+                var youTubeId = youTubeMatch.Groups[1].Value;
+                return $"{SocialMediaConstants.YouTubeEmbedUrl}{youTubeId}?rel=0";
+            }
+
+            var vimeoMatch = Regex.Match(url, SocialMediaConstants.RegexConstants.Vimeo);
+
+            if (vimeoMatch.Success)
+            {
+                var vimeoId = vimeoMatch.Groups[4].Value;
+                return $"{SocialMediaConstants.VimeoEmbedUrl}{vimeoId}";
+            }
+
+            return string.Empty;
+        }
+    }
+}
